f6630e673fdad6f8dfe5f542b1c8a64bd4e07be2
GrazieRcha <graziellyrocha437@gmail.com>
2023-11-30 15:02:26 -0400

    AddMaps

--- src/pages/mapa.js
+++ src/pages/mapa.js
@@ -1,19 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
-
-
+import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 
 export default function Mapa() {
   const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
 
   async function requestForegroundPermissions() {
-    const { granted } = await requestForegroundPermissionsAsync();
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
 
-    if (granted) {
-      const currentPosition = await getCurrentPositionAsync();
-      setLocation(currentPosition);
-      console.log("loc atual", currentPosition);
+      if (granted) {
+        try {
+          const locationWatcher = await watchPositionAsync(
+            { accuracy: Accuracy.High, timeInterval: 1000, distanceInterval: 1 },
+            (newPosition) => {
+              console.log("Nova localização:", newPosition);
+              setLocation(newPosition);
+            }
+          );
+
+          // Lembre-se de limpar o watcher quando o componente for desmontado
+          return () => locationWatcher.remove();
+        } catch (locationError) {
+          console.error("Erro ao obter localização:", locationError);
+          setError("Erro ao obter localização");
+        }
+      } else {
+        console.error("Permissão de localização não concedida.");
+        setError("Permissão de localização não concedida");
+      }
+    } catch (permissionError) {
+      console.error("Erro ao obter permissões de localização:", permissionError);
+      setError("Erro ao obter permissões de localização");
     }
   }
 
@@ -21,10 +41,38 @@ export default function Mapa() {
     requestForegroundPermissions();
   }, []);
 
+  if (error) {
+    return <Text>{error}</Text>;
+  }
+
+  if (!location) {
+    return <ActivityIndicator size="large" />;
+  }
+
   return (
     <View style={styles.mapsContainer}>
-    
-
+      {location && location.coords ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.02,
+            longitudeDelta: 0.02,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Sua Localização"
+            description={`Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`}
+          />
+        </MapView>
+      ) : (
+        <Text>Carregando...</Text>
+      )}
     </View>
   );
 }
@@ -32,14 +80,9 @@ export default function Mapa() {
 const styles = StyleSheet.create({
   mapsContainer: {
     flex: 1,
-   
     backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  map:
-  {
+  map: {
     flex: 1,
-    width: '100%',
-  }
+  },
 });
